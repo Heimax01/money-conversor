e448f54d222e4523fa58fabbf35733141e3b608a
Heitor Cordeiro <heitor_campeao@hotmail.com>
2019-12-22 00:55:58 -0200

    Adicionando tela Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,28 +3,37 @@ import FacebookLogin from 'react-facebook-login';
 
 import GoogleLogin from 'react-google-login';
 
+import './style.css';
+
 // import { Container } from './styles';
 
-export default function Login() {
+export default function Login({history}) {
 
     const responseFacebook = (response) => {
-        console.log(response);
+        history.push('/Conversor');
       }
   
       const responseGoogle = (response) => {
+        history.push('/Conversor');
+      }
+
+      const responseGoogleError = (response) => {
         console.log(response);
       }
 
-
   return (
-    <>
+    <div className="Login">
     
-    <h1>LOGIN COM FACEBOOK OU GOOGLE</h1>
+    <h1>Login com Facebook ou Google</h1>
+        <br />
+        <br />
+
 
         <FacebookLogin
         appId="4102776476459877"
         fields="name,email,picture"
         callback={responseFacebook}
+        textButton="LOGIN COM FACEBOOK"
         />
         <br />
         <br />
@@ -32,11 +41,11 @@ export default function Login() {
 
         <GoogleLogin
         clientId="40775526312-dmc1moom94vrsgm0pp57uogo5k0v54hq.apps.googleusercontent.com" 
-        buttonText="LOGIN WITH GOOGLE"
+        buttonText="LOGIN COM GOOGLE"
         onSuccess={responseGoogle}
-        onFailure={responseGoogle}
+        onFailure={responseGoogleError}
         />
 
-    </>
+    </div>
   );
 }
